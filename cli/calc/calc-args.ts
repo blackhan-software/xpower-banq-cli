b8237c32b71042fdacc41d5913b2405abc5ce467
Kârūn The Rich <development@xpowermine.com>
2024-09-02 17:51:16 +0300

    cli: calc commands

---
+++ cli/calc/calc-args.ts
@@ -0,0 +1,23 @@
+import { parseArgs } from "@std/cli/parse-args";
+import { hex2bigint, suffix_n } from "../../arg/parser.ts";
+
+const CLI_OPTIONS = {
+  boolean: [
+    "version",
+    "help",
+  ],
+  alias: {
+    help: "h",
+    version: "v",
+  },
+};
+
+/**
+ * Parse CLI arguments: hex-literals are treated as bigints.
+ */
+export function parse(strings: string[]) {
+  const { _, ...argv } = parseArgs(suffix_n(strings), CLI_OPTIONS);
+  return Object.assign(argv, { rest: hex2bigint(_) }); // _ => rest
+}
+
+export type CalcArgs = ReturnType<typeof parse>;
