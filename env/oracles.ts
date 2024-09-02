a30290e31cd723ad63127a16609db22cddeb2afa
Kârūn The Rich <development@xpowermine.com>
2024-09-02 17:51:16 +0300

    cli: env-tools

---
+++ env/oracles.ts
@@ -0,0 +1,18 @@
+import { assert } from "../function/assert.ts";
+import "../function/dotenv.ts";
+
+export const T000 = BigInt(Deno.env.get("T000_ADDRESS") ?? 0);
+assert(T000, "missing T000_ADDRESS");
+export const T001 = BigInt(Deno.env.get("T001_ADDRESS") ?? 0);
+assert(T001, "missing T001_ADDRESS");
+export const T002 = BigInt(Deno.env.get("T002_ADDRESS") ?? 0);
+assert(T002, "missing T002_ADDRESS");
+export const T003 = BigInt(Deno.env.get("T003_ADDRESS") ?? 0);
+assert(T003, "missing T003_ADDRESS");
+
+export default ({
+  T000,
+  T001,
+  T002,
+  T003,
+});
