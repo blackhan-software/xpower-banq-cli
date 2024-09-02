e1e9218da6ed141993b4ab994a66032a97438ab1
Kârūn The Rich <development@xpowermine.com>
2024-09-02 17:51:16 +0300

    cli: arg-tools

---
+++ arg/types.ts
@@ -0,0 +1,16 @@
+export type Argument = string | number | bigint | boolean;
+
+export class ArgumentError extends Error {
+  constructor(message: string, options?: ErrorOptions) {
+    super(message, options);
+    this.name = "ArgumentError";
+  }
+}
+export type Page = {
+  page: number | null;
+  page_size: number | null;
+  page_step: number | null;
+};
+export type Mode =
+  | "supply"
+  | "borrow";
