aa7bd938f8ba702fffedc9b22e85d03170260674
Kârūn The Rich <development@xpowermine.com>
2024-09-02 17:51:16 +0300

    cli: arg-tools

---
+++ arg/arg-amount.ts
@@ -0,0 +1,14 @@
+import { type Argument, ArgumentError } from "./types.ts";
+
+export function arg_amount(
+  rest: Argument[],
+): number {
+  const arg = rest.shift();
+  if (typeof arg === "undefined") {
+    throw new ArgumentError("insufficient arguments");
+  }
+  if (typeof arg !== "number" || arg < 0) {
+    throw new ArgumentError(`invalid amount: ${arg}`);
+  }
+  return arg;
+}
