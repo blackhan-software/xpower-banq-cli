94690c824197cdfd0ee5fc5dfa8cf49d360c1b74
Kârūn The Ri₺ch <development@xpowermine.com>
2024-09-02 17:51:16 +0300

    cli: signer-wallet

---
+++ wallet/pk-wallet.ts
@@ -0,0 +1,37 @@
+import { opt_private_key } from "../arg/opt-private-key.ts";
+import { opt_provider_url } from "../arg/opt-provider-url.ts";
+
+import { assert } from "../function/assert.ts";
+import { ethers } from "ethers";
+
+import type { SignerAccount } from "./types.ts";
+export type { SignerAccount } from "./types.ts";
+
+export type SignerOptions = Partial<{
+  provider_url: string;
+  private_key: string;
+}>;
+
+/**
+ * @returns a signer-wallet and its account-address
+ */
+export async function wallet(
+  options?: SignerOptions,
+): Promise<SignerAccount> {
+  const url = opt_provider_url(options);
+  const pk = opt_private_key(options);
+  ///
+  /// construct signer-account
+  ///
+  const provider = url.match(/wss?:\/\//i)
+    ? new ethers.WebSocketProvider(url)
+    : new ethers.JsonRpcProvider(url);
+  if (provider instanceof ethers.WebSocketProvider) {
+    globalThis.addEventListener("unload", () => provider.destroy());
+  }
+  const wallet = new ethers.Wallet(pk, provider);
+  const account = await wallet.getAddress();
+  assert(account, "missing signer-account");
+  ///
+  return { signer: wallet, account };
+}
