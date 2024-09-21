58c479c423ea3057d231dff226d6ec82c9f35389
Kârūn The Rich <development@xpowermine.com>
2024-09-21 13:04:51 +0300

    cli: tab-completion

---
+++ npm/postuninstall.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2015
+###############################################################################
+
+# if [ -n "$(command -v pkg-config)" ] ; then
+#     FOLDER="$(pkg-config --variable=completionsdir bash-completion)" ;
+#     SCRIPT="$FOLDER/banq" ;
+#     if [ -f "$SCRIPT" ] ; then
+#         rm -f "$SCRIPT" 2>/dev/null || true ;
+#     fi
+# fi
+
+###############################################################################
+# bash-completion
+
+TGT_FILE="$HOME/.bash_completion"
+TMP_FILE="/tmp/.bash_completion-$RANDOM"
+
+if [ -f "$TGT_FILE" ]; then
+    grep -v banq <"$TGT_FILE" >"$TMP_FILE" 2>/dev/null || true
+    [ -f "$TMP_FILE" ] && mv "$TMP_FILE" "$TGT_FILE" 2>/dev/null || true
+fi
+
+###############################################################################
+# zsh-completion
+
+TGT_PATH="/usr/local/share/zsh/site-functions"
+TGT_FILE="$TGT_PATH/_banq"
+
+if [ -f "$TGT_FILE" ]; then
+    rm -f "$TGT_FILE" 2>/dev/null || true
+fi
+
+###############################################################################
+###############################################################################
