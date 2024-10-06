ce61596bb6d0ad06ca724d357b720dce81f5c439
Kârūn The Rich <development@xpowermine.com>
2024-10-06 22:00:52 +0300

    cli: banq commands

---
+++ cli/banq/banq-types.ts
@@ -0,0 +1,14 @@
+export type Command =
+  | "pass"
+  | "supply"
+  | "borrow"
+  | "settle"
+  | "redeem"
+  | "liquidate"
+  | "refresh"
+  | "retwap"
+  | "health-of"
+  | "rates-of"
+  | "xpow-init"
+  | "xpow-mine"
+  | "xpow-mint";
