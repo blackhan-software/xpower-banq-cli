9f2b2e561f028eb8dba6779d8c5027c365835781
Kârūn The Rich <development@xpowermine.com>
2025-07-04 17:29:39 +0300

    cli: docker containers

---
+++ docker/xpowerbanq/datasette/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build -t xpowerbanq/datasette -f docker/xpowerbanq/datasette/Dockerfile .
---
+++ docker/xpowerbanq/reindex/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build -t xpowerbanq/reindex -f docker/xpowerbanq/reindex/Dockerfile .
