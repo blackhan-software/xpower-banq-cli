bd75576260f51cc1fc2f11702a5ae9ac275f8d90
Kârūn The Rich <development@blackhan.com>
2025-11-06 16:19:23 +0300

    cli: env-tools

--- env/oracles.ts
+++ env/oracles.ts
@@ -9,10 +9,19 @@ export const T002 = BigInt(Deno.env.get("T002_ADDRESS") ?? 0);
 assert(T002, "missing T002_ADDRESS");
 export const T003 = BigInt(Deno.env.get("T003_ADDRESS") ?? 0);
 assert(T003, "missing T003_ADDRESS");
+export const T004 = BigInt(Deno.env.get("T004_ADDRESS") ?? 0);
+assert(T004, "missing T004_ADDRESS");
+export const T005 = BigInt(Deno.env.get("T005_ADDRESS") ?? 0);
+assert(T005, "missing T005_ADDRESS");
+export const T006 = BigInt(Deno.env.get("T006_ADDRESS") ?? 0);
+assert(T006, "missing T006_ADDRESS");
 
 export default ({
   T000,
   T001,
   T002,
   T003,
+  T004,
+  T005,
+  T006,
 });
--- env/pools.ts
+++ env/pools.ts
@@ -9,10 +9,19 @@ export const P002 = BigInt(Deno.env.get("P002_ADDRESS") ?? 0);
 assert(P002, "missing P002_ADDRESS");
 export const P003 = BigInt(Deno.env.get("P003_ADDRESS") ?? 0);
 assert(P003, "missing P003_ADDRESS");
+export const P004 = BigInt(Deno.env.get("P004_ADDRESS") ?? 0);
+assert(P004, "missing P004_ADDRESS");
+export const P005 = BigInt(Deno.env.get("P005_ADDRESS") ?? 0);
+assert(P005, "missing P005_ADDRESS");
+export const P006 = BigInt(Deno.env.get("P006_ADDRESS") ?? 0);
+assert(P006, "missing P006_ADDRESS");
 
 export default ({
   P000,
   P001,
   P002,
   P003,
+  P004,
+  P005,
+  P006,
 });
